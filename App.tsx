11285b994e0bf4bf941d5b352f012ae28e00529c
Dara Javaherian <darajavaherian@gmail.com>
2021-02-12 17:55:59 +0000

    Make repo more minimal. Remove animation

--- App.tsx
+++ App.tsx
@@ -5,22 +5,13 @@ import {
   StyleSheet,
   Text,
   View,
-  Animated,
   Dimensions,
 } from "react-native";
 
 const styles = StyleSheet.create({
-  container: {
-    width: "100%",
-    height: "100%",
-    display: "flex",
-    flexDirection: "row",
-    backgroundColor: "#fbfbfb",
-  },
   sidePanel: {
     width: Dimensions.get("window").width * 0.7,
     borderBottomWidth: 1,
-    borderBottomColor: "#bbb",
   },
   mainContent: {
     width: Dimensions.get("window").width,
@@ -53,7 +44,7 @@ const SidePanel: React.FC = () => {
 //@ts-ignore
 const Item = ({ transform, toggleOrdersOpen }) => {
   return (
-    <Animated.View
+    <View
       style={[
         styles.testContainer,
         {
@@ -67,69 +58,45 @@ const Item = ({ transform, toggleOrdersOpen }) => {
     >
       <View style={styles.mainContent}>
         <Pressable onPress={toggleOrdersOpen}>
-          <Text>Visible, click me to see a horribly delayed animation</Text>
+          <Text>Visible, click me to see a lot of lag</Text>
         </Pressable>
       </View>
       <View style={styles.sidePanel}>
         <SidePanel />
       </View>
-    </Animated.View>
+    </View>
   );
 };
 
 export default function App() {
   const [sidePanelOpen, setSidePanelOpen] = useState<boolean>(false);
-
-  const [slideAnim, setSlideAnim] = useState(new Animated.Value(0));
-  const [toValue, setToValue] = useState(0);
-
-  const duration = 200;
-
-  useEffect(() => {
-    Animated.timing(slideAnim, {
-      toValue,
-      duration,
-      useNativeDriver: true,
-    }).start();
-
-    // Enable following line and remove preceeding line to remove animation.
-    // I have also tried using no animations whatsoever (just numbers for translation)
-    // And it still has the same delay
-
-    // slideAnim.setValue(toValue)
-  }, [slideAnim]);
+  const [sidePosition, setSidePosition] = useState(0);
 
   const sidePanelWidth = Dimensions.get("window").width * 0.7;
 
   const openSidePanel = () => {
-    setToValue(-sidePanelWidth);
-    setSlideAnim(new Animated.Value(0));
+    setSidePosition(0);
   };
 
   const closeSidePanel = () => {
-    setToValue(0);
-    setSlideAnim(new Animated.Value(-sidePanelWidth));
+    setSidePosition(-sidePanelWidth);
   };
 
-  const setSidePanelState = () => {
+  useEffect(() => {
     if (sidePanelOpen) {
       openSidePanel();
     } else {
       closeSidePanel();
     }
-  };
-
-  useEffect(() => {
-    setSidePanelState();
   }, [sidePanelOpen]);
 
   return (
-    <>
-      <ScrollView style={styles.container}>
-        {Array.from(Array(100)).map((_, i) => {
+    <View style={{ flex: 1 }}>
+      <ScrollView>
+        {Array.from(Array(200)).map((_, i) => {
           return (
             <Item
-              transform={slideAnim}
+              transform={sidePosition}
               key={i}
               toggleOrdersOpen={() => {
                 setSidePanelOpen(!sidePanelOpen);
@@ -138,6 +105,6 @@ export default function App() {
           );
         })}
       </ScrollView>
-    </>
+    </View>
   );
 }

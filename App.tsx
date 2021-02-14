0e1c27c6bf2b1c169a87e8e1a5e04164719c57fd
Dara Javaherian <darajavaherian@gmail.com>
2021-02-14 03:51:11 +0000

    Try without any translation at all

--- App.tsx
+++ App.tsx
@@ -1,30 +1,11 @@
 import React, { useEffect, useState } from "react";
-import {
-  Pressable,
-  ScrollView,
-  StyleSheet,
-  Text,
-  View,
-  Dimensions,
-} from "react-native";
+import { Pressable, ScrollView, StyleSheet, Text, View } from "react-native";
 
 const styles = StyleSheet.create({
-  sidePanel: {
-    width: Dimensions.get("window").width * 0.7,
-    borderBottomWidth: 1,
-  },
   mainContent: {
-    width: Dimensions.get("window").width,
+    width: "100%",
     borderBottomWidth: 1,
     borderBottomColor: "#bbb",
-  },
-  sidePanelContainer: {
-    display: "flex",
-    width: "100%",
-    height: "100%",
-    padding: 15,
-    flex: 1,
-    backgroundColor: "hotpink",
   },
   testContainer: {
     // width: ,
@@ -33,73 +14,42 @@ const styles = StyleSheet.create({
   },
 });
 
-const SidePanel: React.FC = () => {
-  return (
-    <View style={styles.sidePanelContainer}>
-      <Text>HIDDEN</Text>
-    </View>
-  );
-};
-
 //@ts-ignore
-const Item = ({ transform, toggleOrdersOpen }) => {
+const Item = ({ toggleOrdersOpen, val }) => {
   return (
-    <View
-      style={[
-        styles.testContainer,
-        {
-          transform: [
-            {
-              translateX: transform,
-            },
-          ],
-        },
-      ]}
-    >
+    <View style={styles.testContainer}>
       <View style={styles.mainContent}>
         <Pressable onPress={toggleOrdersOpen}>
-          <Text>Visible, click me to see a lot of lag</Text>
+          <Text>Visible, click me to see a lot of lag {val}</Text>
         </Pressable>
-      </View>
-      <View style={styles.sidePanel}>
-        <SidePanel />
       </View>
     </View>
   );
 };
 
 export default function App() {
-  const [sidePanelOpen, setSidePanelOpen] = useState<boolean>(false);
-  const [sidePosition, setSidePosition] = useState(0);
+  const getValues = () => {
+    const arr: any = [];
 
-  const sidePanelWidth = Dimensions.get("window").width * 0.7;
-
-  const openSidePanel = () => {
-    setSidePosition(0);
-  };
-
-  const closeSidePanel = () => {
-    setSidePosition(-sidePanelWidth);
-  };
-
-  useEffect(() => {
-    if (sidePanelOpen) {
-      openSidePanel();
-    } else {
-      closeSidePanel();
+    for (let i = 0; i < 1000; i++) {
+      arr.push(Math.random());
     }
-  }, [sidePanelOpen]);
+
+    return arr;
+  };
+
+  const [values, setValues] = useState<any>(getValues());
 
   return (
     <View style={{ flex: 1 }}>
       <ScrollView>
-        {Array.from(Array(200)).map((_, i) => {
+        {Array.from(Array(500)).map((_, i) => {
           return (
             <Item
-              transform={sidePosition}
               key={i}
+              val={values[i]}
               toggleOrdersOpen={() => {
-                setSidePanelOpen(!sidePanelOpen);
+                setValues(getValues());
               }}
             />
           );
